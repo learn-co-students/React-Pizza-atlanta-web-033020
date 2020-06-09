5953c7e86399bf945c9b0b52faab99de285a4866
rpdecks <rpdecks@gmail.com>
2020-06-08 23:43:31 -0400

    Done.

--- src/containers/PizzaList.js
+++ src/containers/PizzaList.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
 import Pizza from '../components/Pizza'
-class PizzaList extends Component {
 
-  render() {
-    return (
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">Topping</th>
-            <th scope="col">Size</th>
-            <th scope="col">Vegetarian?</th>
-            <th scope="col">Edit</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            //render Pizza here
-          }
-        </tbody>
-      </table>
-    );
+const PizzaList = (props) => {
+  let pizzas = props.pizzas
+  function mapPizzas() {
+    return pizzas.map(pizza => <Pizza 
+      pizza={pizza} 
+      editPizza={props.editPizza} 
+      submitPizzaEdit={props.submitPizzaEdit}
+    />
+    )      
   }
 
+  return (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Topping</th>
+          <th scope="col">Size</th>
+          <th scope="col">Vegetarian?</th>
+          <th scope="col">Edit</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          mapPizzas(props)
+        }
+      </tbody>
+    </table>
+  );
 }
 
 export default PizzaList;
